34b009fdc923e121060e88611a6097dfbfb587f1
sandhya2304 <sandhyasharma0423@gmail.com>
2020-08-20 17:39:36 +0530

    filter

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import data from './data.json';
 import Products from './components/Products';
+import Filter from './components/Filter';
 
 
 class App extends React.Component {
@@ -13,16 +14,57 @@ class App extends React.Component {
     }
   }
 
+  sortProducts = (event) => {
+    const sort = event.target.value;
+       console.log(event.target.value);
+       this.setState((state) => ({
+         sort: sort,
+         products: this.state.products.slice().sort((a,b) =>(
+           sort === "Lowest" ?
+           ((a.price > b.price)? 1 : -1):
+           sort === "Highest" ?
+           ((a.price < b.price)? 1 : -1):
+           ((a._id < b._id)? 1 : -1)
+         ))
+       }));
+}
+
+filterProducts =(event) =>{
+  console.log(event.target.value);
+
+  if(event.target.value === ""){
+    this.setState({size: event.target.value,product: data.products});
+  }else{
+
+    this.setState({
+      size: event.target.value,
+      products: data.products.filter(
+        (product) => product.availableSize.indexOf(event.target.value) >=0
+      ),
+    });
+
+  }
+
+  
+}
   render(){
 
   return (
     <div className="grid-container">
         <header>
-           <a href="/">React Shopping cart</a>
+           <a href="/">Shopping cart</a>
         </header>
         <main>
             <div className="content">
               <div className="main">
+                  <Filter 
+                  count={this.state.products.length}
+                  size={this.state.size}
+                  sort={this.state.sort}
+                  filterProducts={this.filterProducts}
+                  sortProducts={this.sortProducts}
+                  />
+
                   <Products products={this.state.products} />
               </div>
               <div className="sidebar">
@@ -31,7 +73,7 @@ class App extends React.Component {
             </div>
         </main>
         <footer>
-            @Shppingsite
+            @Shoppingsite
         </footer>
     </div>
   );
